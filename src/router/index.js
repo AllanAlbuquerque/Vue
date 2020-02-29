7414377da65b2fe6b761ffb9e21f73fdb0d36d13
intelity <jira@intelity.com.br>
2020-02-29 19:31:22 -0300

    Primeiro commit master

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: () => import('../views/EventList.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    props: true,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EventShow.vue')
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: () => import('../views/EventCreate.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+export default router;
